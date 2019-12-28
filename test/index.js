9e9b988225f1d966c8732701b2b2d839f6cfb00b
Varun <28918411+VarunDevPro@users.noreply.github.com>
2019-12-28 12:12:24 +0530

    first commit

---
+++ test/index.js
@@ -0,0 +1,11 @@
+const colors = require("../lib");
+colors.black("This should be printed in black");
+colors.red("This should be printed in red");
+colors.green("This should be printed in green");
+colors.yellow("This should be printed in yellow");
+colors.blue("This should be printed in blue");
+colors.magenta("This should be printed in magenta");
+colors.cyan("This should be printed in cyan");
+colors.white("This should be printed in white");
+colors.gray("This should be printed in gray");
+colors.grey("This should be printed in grey");
